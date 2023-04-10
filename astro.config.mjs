4b13dc7baef1c6e9a824806478156842acb64f17
tostu <torge.stubbe@googlemail.com>
2023-04-10 17:53:45 +0200

    deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://https://tostu.github.io',
+  base: '/astro-blog',
   integrations: [mdx(), sitemap(), tailwind()]
 });
