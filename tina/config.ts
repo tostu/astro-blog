1587a7e1a87a302e5ba5298cb89d13867b437dd7
tostu <torge.stubbe@googlemail.com>
2023-04-10 18:21:38 +0200

    base path + tina config

--- tina/config.ts
+++ tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: '63a52104-dfa1-4fb2-96f1-916e9d7c5099', // Get this from tina.io
+  token: '6705255e3dec1c2cf8b11d1ca7443a23fca1b2dd', // Get this from tina.io
 
   build: {
     outputFolder: "admin",
